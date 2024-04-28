00145a24deca16dc4aafbfd258851f62e50019ef
Edgar <edgarasige@gmail.com>
2024-04-28 20:10:38 +0300

    Completed assingments

--- src/TransactionForm.js
+++ src/TransactionForm.js
@@ -7,3 +7,34 @@ const TransactionForm = ({ addTransaction }) => {
         const description = event.target[1].value; 
         const category = event.target[2].value;
         const amount = event.target[3].value; 
+
+        if (!date || !description || !category || !amount) {
+            alert("Please fill in all the input fields before adding a transaction");
+            return;
+          }
+      
+          const newTransaction = {
+            date,
+            description,
+            category,
+            amount
+          };
+          addTransaction(newTransaction);
+          event.target.reset();
+        };
+      
+        return (
+          <div id="options">
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder="mm / dd / yyyy" />
+              <input type="text"  placeholder="Description" />
+              <input type="text" placeholder="Category" />
+              <input type="number" placeholder="Amount" />
+              <button id="transaction" type="submit">Add Transaction</button>
+            </form>
+          </div>
+        );
+      };
+      
+      export default TransactionForm;
+      
